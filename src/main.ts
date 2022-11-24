ed6b7b04feee98c96aef47a477b2c805633f1f0c
Matheus Mantini <engmatheusmantini@gmail.com>
2022-11-24 09:52:20 -0300

    Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { dailyCron } from './utils/daily-cron';
 import { saveApiDataToDB } from './utils/save-api-data';
+import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocs from './swagger/swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
   saveApiDataToDB();
 
